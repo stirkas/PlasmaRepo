e7c860e9f366d530f0cf8ca93f11f0787bde2b22
Stefan Tirkas <stefan.tirkas@colorado.edu>
2020-02-17 14:49:20 -0700

    plot/data entry update

--- geneScripts/plotPhi.py
+++ geneScripts/plotPhi.py
@@ -3,25 +3,27 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 nt = 509
-nx = 194
-ny = 33
-nkx = nx-2 #GENE seems to drop size by 2 for k-space. 
-nky = ny-2 #GENE seems to drop size by 2 for k-space.
+nx = 192 + 1 #GENE uses 2 gridpoints per mode + origin(0).
+nky = 16
+nkx = nx - 1 #Not sure why GENE data has 1 less point for nkx.
+ny = nky*2 + 1 #GENE uses 2 gridpoints per mode + origin(0).
 lx = 5.63558
 ly = 2.96377
 t = x = y = 0
 phit = np.zeros((nt,nx,ny)) #[t,x,y]
-phikt = np.zeros((nt,nkx,nky))
+phikt = np.zeros((nt,nkx,2*nky - 1)) #Not sure why GENE data has less points in k space.
 readingData = False
 currentlySaving = False
 showPlot = False
 xp = np.linspace(-lx/2, lx/2, nx)
 yp = np.linspace(-ly/2, ly/2, ny)
-xkp = (2*np.pi*nkx/lx)*np.linspace(-1/2+(1/nkx),1/2,nkx) #For some reason x needs to be offset by 1.
-ykp = (2*np.pi*nky/ly)*np.linspace(-1,1,nky) #Need to double the range for y. Apparently GENE does the full range of modes positive, then reflects.
+xkp = (2*np.pi*nkx/lx)*np.linspace(-1/2,1/2,nkx) #For some reason x needs to be offset by 1.
+ykp = (2*np.pi*nky/ly)*np.linspace(-1,1,2*nky-1) #Need to double the range for y. Apparently GENE does the full range of modes positive, then reflects.
 #Begin loading data.
 fileName = '/home/stirkas/Workspace/GENE/Output/phi_21-24_real.dat'
 f = open(fileName, 'r')
+
+count = 0
 
 for line in f.readlines():
    if (line[0] == '#'):
@@ -32,7 +34,7 @@ for line in f.readlines():
       if (t == 0): readingData = True
       if (len(line.split()) > 0): #Ignore empty lines. Not sure why there are x-values with no corresponding y sometimes.
          phit[t,x,:] = line.split()
-      x = x + 1
+      x = x+1
 f.close()
 
 readingData = False
