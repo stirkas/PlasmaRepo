63046aec4bfa776380972cdcffe3378fa1bd2258
Stefan Tirkas <stefan.tirkas@colorado.edu>
2020-03-31 14:57:49 -0600

    Added zonal flow cases.

---
+++ geneScripts/GoerlerZonal/runCase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 ../plotPhi.py 331 192 2 5.07202 0.987922 './phi_0026_r.dat' './phi_0026_k.dat' -s -f './phiZonal3kymin.mp4'
+
+#python3 ../plotPhi.py 248 192 2 5.07202 .592753 './phi_0027_r.dat' './phi_0027_k.dat' -s -f './phiZonal5kymin.mp4'
+
